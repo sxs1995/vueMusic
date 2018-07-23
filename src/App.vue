e399f59c187ce92fb4423c0d53659d8059f882c2
sxs1995 <991956448@qq.com>
2018-07-23 14:07:38 +0800

    首页

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Mheader></Mheader>
+    <Tab></Tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import Mheader from '@/components/m-header/m-header'
+import Tab from '@/components/tab/tab'
+export default {
+  name: 'App',
+  components: {
+    Mheader,
+    Tab
+  }
+}
+</script>
+
+<style>
+</style>
