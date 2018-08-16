efa1e3eae9e77048892f9cda797d4ba1eff6198c
Sxians <l>
2018-08-16 15:25:30 +0800

    歌手页面右侧入口开发

--- src/api/search.js
+++ src/api/search.js
@@ -1,7 +1,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
-export function getHotKey() {
+export function getHotKey () {
   const url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
 
   const data = Object.assign({}, commonParams, {
@@ -13,7 +13,7 @@ export function getHotKey() {
   return jsonp(url, data, options)
 }
 
-export function search(query, page, zhida, perpage) {
+export function search (query, page, zhida, perpage) {
   const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
 
   const data = Object.assign({}, commonParams, {
